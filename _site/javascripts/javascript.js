ef404c27ce986d2091555d79973e8c4604d9fb0b
kohkimakimoto <kohki.makimoto@gmail.com>
2013-07-05 17:26:07 +0900

    Update pages

--- _site/javascripts/javascript.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $("pre.php").snippet("php",{style:"vim-dark"});
-  $("pre.javascript").snippet("javascript",{style:"vim-dark"});
-  $("pre.sh").snippet("sh",{style:"vim-dark"});
-});
